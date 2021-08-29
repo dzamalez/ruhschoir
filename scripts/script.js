46d082c1ff091ad935e185b5152634ead627aa70
dzamalez <diego.zamora193@gmail.com>
2021-08-28 22:15:06 -0700

    added alert with end date

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,4 @@
+/*
 function openNav() {
     document.getElementById("mySidenav").style.width = "250px";
     document.getElementById("main").style.marginLeft = "250px";
@@ -9,3 +10,14 @@ function openNav() {
     document.getElementById("main").style.marginLeft= "0";
     document.body.style.backgroundColor = "white";
   }
+*/
+const d = new Date();
+let currentDay = d.getDate();
+let currentMonth = d.getMonth();
+let currentDate = `${currentMonth} ${currentDay}`;
+console.log(`Month: ${currentMonth}`)
+console.log(`Day: ${currentDay}`)
+
+if (currentDay <= 8 && currentMonth <= 8 ) {
+  alert("Hey there! The is a fundrasier currently going on, please visit the fundraisers under the support us tab!")
+} 
