b55a9cc7da7dd88f294b1e892530dc6f9ce121f6
dzamalez <diego.zamora193@gmail.com>
2021-09-11 21:58:44 -0700

    removed alert

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,4 @@
-/*
+// mobile nav menu
 function openNav() {
     document.getElementById("mySidenav").style.width = "250px";
     document.getElementById("main").style.marginLeft = "250px";
@@ -10,14 +10,5 @@ function openNav() {
     document.getElementById("main").style.marginLeft= "0";
     document.body.style.backgroundColor = "white";
   }
-*/
-const d = new Date();
-let currentDay = d.getDate();
-let currentMonth = d.getMonth();
-let currentDate = `${currentMonth} ${currentDay}`;
-console.log(`Month: ${currentMonth}`)
-console.log(`Day: ${currentDay}`)
 
-if ((currentDay <= 8 && currentMonth <= 8) || (currentDay > 8 && currentMonth <= 8)) {
-  alert("Hey there! The is a fundrasier currently going on, please visit the fundraisers under the support us tab!")
-} 
+// alert system for the fundrasier 
